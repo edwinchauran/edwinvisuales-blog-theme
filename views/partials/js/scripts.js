802aa9c36170fc3dda97c650907875c0248975ef
edwinvisuales <thedwinvisual@gmail.com>
2020-12-05 01:25:56 -0400

    fix highlights responsive and recientes

--- views/partials/js/scripts.js
+++ views/partials/js/scripts.js
@@ -1,1 +1,16 @@
+//HIGHLIGHTS
+let highlights = document.getElementsByClassName('highlight');
+let info = document.getElementsByClassName('info');
+//change highlight info color to yellow when mouseover
+for(let i = 0;  highlights.length > i; i++){
+
+    highlights[i].addEventListener('mouseover',function() {
+        info[i].style.color = "black";
+        info[i].style.backgroundColor = "rgba(251, 255, 0, 0.75)";  
+    })
+    highlights[i].addEventListener('mouseleave', function() {
+        info[i].style.color = "white";
+        info[i].style.backgroundColor =   "rgba(0, 0, 0, 75%)"; 
+    })
+}
 
